c7a701914bb2b2eb326f1d0d1adb480dbda54201
Madhusmita Barik <162332770+Madhusmita7894@users.noreply.github.com>
2024-03-16 21:17:04 -0700

    Add files via upload
    Operators in python

---
+++ 04.py
@@ -0,0 +1,58 @@
+#operator
+#Arithmetic operator
+#example:-1
+a=10
+b=2
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a**b)
+print(a//b)
+#example:-2
+branch='cse'
+print(branch+'60')
+print(branch*3)
+#Assignment operator
+#example:-1
+a=b=50
+print(a,b)
+a,b=50,60
+print(a,b)
+a=10
+a+=30
+print(a)
+#Relational operator
+#example:-1
+a=10
+b=20
+print(a<b)
+print(a>b)
+print(a<=b)
+print(a>=b)
+print(a==b)
+print(a!=b)
+#logical operator
+#example:-1
+print(10 and 15)
+print(0 and 15)
+print(10 and 15)
+print(0 and 15)
+#special operator
+#1.identity operator
+a=50
+b=50
+print(a is b)
+#conditional statement in python
+#example of if:-
+mark=int(input("enter mark"))
+if mark<33:
+    print("you are fail")
+if mark>33:
+    print("you are pass")
+
+
+
+
+
